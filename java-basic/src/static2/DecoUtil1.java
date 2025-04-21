61eee4fcdfbdafa0ffe6c32c21e285f085585917
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-04-22 01:40:53 +0900

    section 8 : finish static method

---
+++ java-basic/src/static2/DecoUtil1.java
@@ -0,0 +1,11 @@
+package static2;
+
+public class DecoUtil1 {
+
+    public String deco(String str) {
+        String result = "*" + str + "*";
+        return result;
+
+        //return "*" + str + "*"; 도 있다 읽기 쉬우라고 영한쌤이 위예시로 작성해준거
+    }
+}
