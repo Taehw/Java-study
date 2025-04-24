815e755ed3f938149e15c7778e981d8995d60a8b
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-04-24 23:22:36 +0900

    section12 - polymorphism, abstract, interface

---
+++ java-basic/src/poly/ex/AnimalSoundMain.java
@@ -0,0 +1,22 @@
+package poly.ex;
+
+public class AnimalSoundMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        System.out.println("동물소리 테스트 시작");
+        dog.sound();
+        System.out.println("동물소리 테스트 시작");
+
+        System.out.println("동물소리 테스트 시작");
+        cat.sound();
+        System.out.println("동물소리 테스트 시작");
+
+        System.out.println("동물소리 테스트 시작");
+        caw.sound();
+        System.out.println("동물소리 테스트 시작");
+    }
+}
---
+++ java-basic/src/poly/ex3/Dog.java
@@ -0,0 +1,10 @@
+package poly.ex3;
+
+import poly.ex2.Animal;
+
+public class Dog extends AbstractAnimal {
+    @Override
+    public void sound() {
+        System.out.println("멍ㅁ어");
+    }
+}
