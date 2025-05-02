51eb2c52c0d9e1ae279f44a8783f5d09ac357678
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-05-02 18:40:11 +0900

    add commnet

--- java-mid1/src/time/ZonedDateTimeMain.java
+++ java-mid1/src/time/ZonedDateTimeMain.java
@@ -10,12 +10,16 @@ public class ZonedDateTimeMain {
         ZonedDateTime nowZdt = ZonedDateTime.now();
         System.out.println("nowZdt = " + nowZdt);
 
+        //localdatetime 기반 zoneddatetime 생성
         LocalDateTime ldt = LocalDateTime.of(2030, 1, 1, 13, 30, 50);
         ZonedDateTime zdt1 = ZonedDateTime.of(ldt, ZoneId.of("Asia/Seoul"));
         System.out.println("zdt1 = " + zdt1);
 
+        //이렇게 생성하는 방법도 있음
         ZonedDateTime zdt2 = ZonedDateTime.of(2030, 1, 1, 13, 30, 50, 0, ZoneId.of("Asia/Seoul"));
         System.out.println("zdt2 = " + zdt2);
+
+        //타임존 변경
         ZonedDateTime utcZdt = zdt2.withZoneSameInstant(ZoneId.of("UTC"));
         System.out.println("utcZdt = " + utcZdt);
 
