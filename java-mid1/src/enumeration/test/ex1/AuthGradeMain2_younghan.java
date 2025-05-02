01aa1b946481b7a7bff99339d7d80841374c1f91
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-05-02 17:39:44 +0900

    section7 - time library example

--- java-mid1/src/enumeration/test/ex1/AuthGradeMain2_younghan.java
+++ java-mid1/src/enumeration/test/ex1/AuthGradeMain2_younghan.java
@@ -2,7 +2,7 @@ package enumeration.test.ex1;
 import java.util.Scanner;
 public class AuthGradeMain2_younghan {
 
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("당신의 등급을 입력하세요[GUEST, LOGIN, ADMIN]: ");
