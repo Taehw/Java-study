30beeaf3327e41e4df84fad0ce035bc28b86295a
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-05-24 21:34:32 +0900

    Feature of array

--- java-mid2/src/collection/array/ArrayMain2.java
+++ java-mid2/src/collection/array/ArrayMain2.java
@@ -2,37 +2,55 @@ package collection.array;
 
 import java.util.Arrays;
 
-public class ArrayMain1 {
+/**
+ * 배열의 특징
+ */
+public class ArrayMain2 {
 
     public static void main(String[] args) {
-
         int[] arr = new int[5];
-
-        //index 입력: O(1)
-        System.out.println("==index 입력: O(1)==");
         arr[0] = 1;
         arr[1] = 2;
-        arr[2] = 3;
         System.out.println(Arrays.toString(arr));
 
-        //index 변경: O(1)
-        System.out.println("==index 변경: O(1)==");
-        arr[2] = 10; System.out.println(Arrays.toString(arr));
+        //배열의 첫번째 위치게 추가
+        //기본 배열의 데이터를 한칸씩 뒤로 밀고 배열의 첫번째 위치에 추가
+        System.out.println("배열의 첫번째 위치에 3 추가 : O(n)");
+        int newValue = 3;
 
-        //index 조회: O(1)
-        System.out.println("==index 조회: O(1)==");
-        System.out.println("arr[2] = " + arr[2]);
-
-        //검색: O(n)
-        System.out.println("==배열 검색: O(n)==");
+        addFrist(arr, newValue);
         System.out.println(Arrays.toString(arr));
-        int value = 10;
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("arr[" + i + "]:" + arr[i]);
-            if (arr[i] == value) {
-                System.out.println(value + " 찾음");
-                break;
-            }
+
+        //index 위치에 추가
+        //기본 배열의 데이터를 한칸씩 뒤로 밀고 배열의 index 위치에 추가
+        System.out.println("배열의 index(2) 위치에 4 추가 : O(n)");
+        int index =2;
+        int value = 4;
+        addAtIndex(arr, index, value);
+
+        //배열의 마지막 위치에 추가
+        System.out.println("배열의 마지막 위치에 5 추가 : O(1)");
+        addLast(arr, 5);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void addLast(int[] arr, int newValue) {
+        arr[arr.length - 1] = newValue;
+    }
+
+    private static void addAtIndex(int[] arr, int index, int newValue) {
+        for (int i = arr.length - 1; i > index; i--) {
+            arr[i] = arr[i - 1];
         }
+        arr[index] = newValue;
+    }
+
+    //첫번째 위치에 추가하기 위해 뒤에 서부터 오른쪽으로 밀기
+    private static void addFrist(int[] arr, int newValue) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = newValue;
     }
 }
