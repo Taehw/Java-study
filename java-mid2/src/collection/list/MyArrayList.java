633c27d7ed15f8c12607ecf72991b1e6e24ffcc0
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-06-03 10:55:31 +0900

    list abstraciton

---
+++ java-mid2/src/collection/list/MyArrayList.java
@@ -0,0 +1,119 @@
+package collection.list;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> implements MyList<E>{
+
+    private static final int DEFAULT_CAPACITY = 5; //배열의 기본 크기
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    //지정한 용야랑으로 배열으 ㅣ크기 설정
+    public MyArrayList(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    //배열의 size를 리턴
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+
+    //배열에 원소를 추가하고 size 늘린다
+    public void add(E e) {
+
+        if (size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+    @Override
+
+    //원하는 위치에 코드 추가
+    public void add(int index, E e) {
+        if (size == elementData.length) {
+            grow();
+        }
+        shiftRightFrom(index); //기존 원소들을 밀고
+        elementData[index] = e; //새로운 원소를 원하는 index위치에 추가
+        size++;
+    }
+
+    //코드 추가 , 요소의 마지막부터 index까지 오른쪽으로 밀기
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index; i--) {
+            elementData[i] = elementData[i - 1];
+        }
+    }
+
+
+    private void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    // 지정된 인덱스의 원소를 반환
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public E get(int index) {
+        return (E) elementData[index];
+    }
+
+    @Override
+    //인덱스와 배열의 원소값을 받아서 예전값은 따로 저장해서 리턴하고 지정된 인덱스의 값을 교체
+    public E set(int index, Object element) {
+        E oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+    //코드 추가
+    //데이터 삭제
+    @Override
+    public E remove(int index) {
+        E oldValue = get(index); //삭제된 위치의 값은 따로 저장해주고
+        shiftLeftFrom(index); //index 위치 오른쪽 원소들을 왼쪽으로 이동해서 채워줘야지
+
+        size--; //삭제햇으니까 size가 줄어들겠지?
+        elementData[size] = null; //마지막에는 null을 채워주기 - 원소값들은 복사후 대입 방식으로 왼쪽으로 밀리기때문에 이게 없으면 abcc가 될수도 있음 c가 null이 되어야한다~
+        return oldValue;
+    }
+
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size - 1; i++) {
+            elementData[i] = elementData[i + 1];
+        }
+    }
+
+    //원소가 배열의 몇번째에 위치했는지를 반환
+    //원소가 들어간 크기만큼만 돌면되서 size 보다 작은 i값을 활용하였음
+    public int indexOf(E o) {
+        for (int i = 0; i < size; i++) {
+            if (o.equals(elementData[i])) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData, size)) + " size = " + size + " , capacity = " + elementData.length;
+    }
+    //copyOf : size의 크기만큼만 출력하고 싶어서 t사용
+    /**
+     * 실제 데이터 1 2 3 null null 이라면 (size = 3)
+     * 사이즈 크기 만큼 카피후 1 2 3 만 출력
+     */
+}
---
+++ java-mid2/src/collection/list/MyLinkedList.java
@@ -0,0 +1,128 @@
+package collection.list;
+
+public class MyLinkedList<E> implements MyList<E>{
+
+    private Node <E> first;
+    private int size = 0;
+
+    @Override
+    public void add(E e) {
+        Node<E> newNode = new Node<>(e);
+        if (first == null) { //최초 노드인 경우
+            first = newNode;
+        } else { //null이 아니면 마지막에 추가하기
+            Node<E> lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+        size++;
+    }
+
+    private Node<E> getLastNode() {
+        Node<E> x = first;
+        while (x.next != null) { //처음부터 탐색해서 next 가 null이 아닐때까지
+            x = x.next;
+        }
+        return x; //next 가 널이면 마지막 node 일거고 x를 반환 -> 머릿속으로 상상하면서 그려보셈
+    }
+
+    //추가 코드
+    @Override
+    public void add(int index, E e) {
+        Node<E> newNode = new Node<>(e); //타입추론에 의해 생략
+        if (index == 0) { //첫번째 위치에 추가
+            newNode.next = first;
+            first = newNode;
+        } else {
+            //중간 위치에 Node를 추가
+            Node<E> prev = getNode(index - 1); //이전노드 찾는거니까 index에서 -1
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+        size++; // 추가하면서 사이즈 늘어나니까
+    }
+
+    //Node에 값을 세팅한우 이번값 반환을 위해 반환타입으로 Object 지정
+    @Override
+    public E set(int index, E element) {
+        Node<E> x = getNode(index); //원하는 인덱스에 있는 노드부터 찾기
+        E oldValue = x.item;
+        x.item = element; //노드값 교체
+        return oldValue;
+    }
+
+    //추가 코드
+    @Override
+    public E remove(int index) {
+        Node<E> removeNode = getNode(index); //삭제할 노드를 찾고
+        E removedItem = removeNode.item;
+        if (index == 0) { //첫번재 노드 삭제
+            first = removeNode.next;
+        } else {
+            //중간 노드 삭제
+            Node<E> prev = getNode(index - 1);
+            prev.next = removeNode.next;
+        }
+        removeNode.item = null;
+        removeNode.next = null;
+        size--; //삭제됐으니까 size는 줄겠지
+
+        return removedItem; //삭제된 원소를 반환
+    }
+
+    @Override
+    public E get(int index) {
+        Node<E> node = getNode(index);
+        return node.item;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> x = first;
+        for(int i= 0 ; i<index ; i++){ //i가 0이면 첫번재 노드 i는 노드사이의 건너뛰기 횟수
+            x = x.next;
+        }
+        return x;
+    }
+
+    public int indexOf(E o) {
+        int index = 0;
+        for (Node<E> x = first; x != null; x = x.next) { //첫번째 노드부터 x의 다음이 있을때까지 리스트를 순회
+            // -> item의 인덱스와 같은 원소가 리스트이 있으면 index를 반환
+            if(o.equals(x.item))
+                return index;
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //증첩클래스 생성
+    private static class Node <E>{
+        E item;
+        Node<E> next;
+
+        public Node(E item) {
+            this.item = item;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            Node<E> temp = this;
+            sb.append("[");
+            while (temp != null) {
+                sb.append(temp.item);
+                if(temp.next !=null){
+                    sb.append("->");
+                }
+                temp = temp.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+
+}
