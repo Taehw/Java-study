e3187e606b25f71b2ec4faed643fa45286a3c888
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-06-05 22:23:26 +0900

    MyHashSetV1 - improve duplicate check

---
+++ java-mid2/src/collection/set/MyHashSetV1.java
@@ -0,0 +1,99 @@
+package collection.set;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class MyHashSetV1 {
+
+    static final int DEFAULT_INITIAL_CAPACITY = 16; //기본 용량이 너무 작으면 해시충돌 많아지니 적절히 설정
+
+    LinkedList<Integer>[] buckets;
+
+    private int size = 0;
+    private int capacity = DEFAULT_INITIAL_CAPACITY;
+
+    //디폴트 용량으로 생성
+    public MyHashSetV1() {
+        initBuckets();
+    }
+
+    //용량을 내가 지정
+    public MyHashSetV1(int capacity) {
+        this.capacity = capacity;
+        initBuckets();
+    }
+
+    //생성자에서 반복적으로 사용되는 부분이라 메서드 추출로 따로 만들어줬다
+    //buckets안의 각 요소를 연결리스트로 초기화 해주는 부분 -> [[],[],[],[]........,[],[]]
+    private void initBuckets() {
+
+        buckets = new LinkedList[capacity];
+        for (int i = 0; i < capacity; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    //이미 해왔던 것들
+    public boolean add(int value) {
+        int hashIndex = hashIndex(value); //해시인덱스부터 구하기
+        LinkedList<Integer> bucket = buckets[hashIndex];
+        if (bucket.contains(value)) {
+            return false;
+        }
+
+        bucket.add(value);
+        size++;
+
+        return true;
+    }
+
+
+    public boolean contains(int searchValue){
+        int hashIndex = hashIndex(searchValue); //여기도 해시인덱스부터 구하고
+        LinkedList<Integer> bucket = buckets[hashIndex]; //해시인덱스의 바구니를 찾음
+        return bucket.contains(searchValue); //바구니안에 찾는값이 있는지 검사, 연결리스트이기때문에 기본 기능으로 contains를 사용한다.
+    }
+
+    //v0에서는 remove는 구현하지 않았다.
+    public boolean remove(int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex];
+
+        boolean result = bucket.remove(Integer.valueOf(value));
+        //연결리스트이 remove의 경우 두가지의 종류가 있음
+        //1. remove(int index)
+        //2. remove(Object o)
+        //아래 예시 참고
+//        List<Integer> list = new ArrayList<>();
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//
+//        list.remove(1);              // ✅ remove(int index) → 20 삭제
+//        list.remove(Integer.valueOf(30)); // ✅ remove(Object o) → 30 삭제
+
+
+        if (result) { //바구니 안에 삭제하려는 요소가 존재했다으면 삭제 ㄱㄱ
+            size--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int hashIndex(int value) {
+        return value % capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyHashSetV1{" +
+                "buckets=" + Arrays.toString(buckets) +
+                ", size=" + size +
+        '}';
+    }
+}
